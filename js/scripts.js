9d65d82783bf16cf451446bf433c795212a0fb65
samuelkaranja <samuelkaranja007@gmail.com>
2020-04-03 12:58:16 +0300

    File update

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,5 @@
-// Pre loader
-window.addEventListener("load", function(event) {
-	$(".se-pre-con").delay(800).hide();
-});
+// Preloader
 
-// get the element to animate
-var element = document.getElementById('abt');
-var elementHeight = element.clientHeight;
-
-// listen for scroll event and call animate function
-document.addEventListener('scroll', animated);
-
-// check if element is in view
-function inView() {
-  // get window height
-  var windowHeight = window.innerHeight;
-  // get number of pixels that the document is scrolled
-  var scrollY = window.scrollY || window.pageYOffset;
-  
-  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
-  var scrollPosition = scrollY + windowHeight;
-  // get element position (distance from the top of the page to the bottom of the element)
-  var elementPosition = element.getBoundingClientRect().top + scrollY + elementHeight;
-  
-  // is scroll position greater than element position? (is element in view?)
-  if (scrollPosition > elementPosition) {
-    return true;
-  }
-  
-  return false;
-}
-
-// animate element when it is in view
-function animate() {
-  // is element in view?
-  if (inView()) {
-      // element is in view, add class to element
-      element.classList.add('animated');
-  }
-}
+setTimeout(function(){
+  $('body').addClass('loaded');
+}, 2000);
